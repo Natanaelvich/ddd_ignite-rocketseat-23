a3f334209d8bd0702cce91085ac10ce3fc2e75d7
Natanaelvich <taelima1997@gmail.com>
2023-04-25 22:28:16 -0300

    add config vitest path

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-/// <reference types="vitest" />
-
-// Configure Vitest (https://vitest.dev/config/)
-
 import { defineConfig } from 'vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  plugins: [tsConfigPaths()],
   test: {
-    /* for example, use global to avoid globals imports (describe, test, expect): */
-    // globals: true,
-  },
+    globals: true
+  }
 })
