cda789a919a5c2022a3bc48d8515aa9e104332e8
Natanaelvich <taelima1997@gmail.com>
2023-05-13 22:35:22 -0300

    update AnswersRepository with more methods

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -2,7 +2,10 @@ import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Answer } from '../../enterprise/entities/answer'
 
 export interface AnswersRepository {
+  findById(id: string): Promise<Answer | null>
   create(answer: Answer): Promise<void>
+  save(answer: Answer): Promise<void>
+  delete(answer: Answer): Promise<void>
   findManyByQuestionId(
     questionId: string,
     params: PaginationParams,
